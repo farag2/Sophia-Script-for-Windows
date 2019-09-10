ca3092f4fe14f164c2d0e5295732961b210202d2
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2019-09-10 17:54:04 +0300

    GUI prototype

---
+++ VS/Win-10-Setup-Script-GUI/Win-10-Setup-Script-GUI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Win_10_Setup_Script_GUI
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
