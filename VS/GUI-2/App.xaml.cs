036d1d5158472a021460c610ca47a2743c31e7a4
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2019-09-19 18:05:35 +0300

    Added new gui project

---
+++ VS/GUI-2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUI_2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
