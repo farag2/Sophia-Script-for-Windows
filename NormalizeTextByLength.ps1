090949e39a8b873cd94cd6a86ce4e1dc339d6eb7
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2019-10-04 15:14:59 +0300

    Version 0.0.2

--- NormalizeTextByLength.ps1
+++ NormalizeTextByLength.ps1
@@ -1,16 +1,5 @@
-$string = $null
-$sourceText = Get-Content -Path "C:\Tmp\text.txt"
-for ($i = 0; $i -lt $sourceText.Count; $i++) {
-    
-    if ($i % 2 -eq 0) {
-        $string += """{0}"","-f $sourceText[$i].Replace("# ","")
-    }
-}
+$text = ""
 
-
-$text = "Add ""Extract"" to MSI file type context menu","Add ""Run as different user"" from context menu for .exe file type","Add ""Install"" to CAB file type context menu","Remove ""Cast to Device"" from context menu","Remove ""Share"" from context menu","Remove ""Previous Versions"" from file context menu","Remove ""Edit with Paint 3D"" from context menu","Remove ""Include in Library"" from context menu","Remove ""Turn on BitLocker"" from context menu","Remove ""Edit with Photos"" from context menu","Remove ""Create a new video"" from Context Menu","Remove ""Edit"" from Context Menu","Remove ""Print"" from batch and cmd files context menu","Remove ""Compressed (zipped) Folder"" from context menu","Remove ""Rich Text Document"" from context menu","Remove ""Bitmap image"" from context menu","Remove ""Send to"" from folder context menu","Make the ""Open"", ""Print"", ""Edit"" context menu items available, when more than 15 selected","Turn off ""Look for an app in the Microsoft Store"" in ""Open with"" dialog"
-
-#region Normalize Text by Length
 $textLength = New-Object System.Collections.ArrayList($null)
 
 for ($i = 0; $i -lt $text.Length; $i++) {    
@@ -21,4 +10,3 @@ for ($i = 0; $i -lt $text.Length; $i++) {
 ($textLength | Group-Object -Property TextLength | Sort-Object -Property Count -Descending).Group.Text -replace """", "&quot;" | ForEach-Object {
     """{0}""," -f $_
 }
-#endregion Normalize Text by Length
--- ToggleButtonsGenerator.ps1
+++ ToggleButtonsGenerator.ps1
@@ -1,213 +1,59 @@
-#region Privacy & Telemetry Normalized Text
-$textPrivacy = "Change Windows Feedback frequency to &quot;Never&quot;",
-"Turn off automatic installing suggested apps",
-"Turn off &quot;Connected User Experiences and Telemetry&quot; service",
-"Turn off the SQMLogger session at the next computer restart",
-"Do not allow apps to use advertising ID",
-"Do not use sign-in info to automatically finish setting up device after an update or restart", "Do not let websites provide locally relevant content by accessing language list",
-"Turn off suggested content in the Settings",
-"Turn off tip, trick, and suggestions as you use Windows",
-"Turn off reserved storage",
-"Do not let apps on other devices open and message apps on this device, and vice versa",
-"Set the operating system diagnostic data level to &quot;Basic&quot;",
-"Turn off the Autologger session at the next computer restart",
-"Turn off per-user services",
-"Do not offer tailored experiences based on the diagnostic data setting",
-"Turn off diagnostics tracking scheduled tasks",
-"Turn off Windows Error Reporting"
-#endregion Privacy & Telemetry Normalized Text
+$currentDir = $MyInvocation.MyCommand.Definition | Split-Path -Parent
+$outFile = "{0}\ToggleButtonsGenerator.txt"-f $currentDir
 
-#region UI & Personalization Normalized Text
-$textUi = "Set the Control Panel view by large icons",
-"Hide search box or search icon on taskbar",
-"Turn off &quot;New App Installed&quot; notification",
-"Turn off automatically hiding scroll bars",
-"Hide all folders in the navigation pane",
-"Hide &quot;Frequent folders&quot; in Quick access",
-"Choose theme color for default app mode",
-"Show File Name Extensions",
-"Show &quot;This PC&quot; on Desktop",
-"Show Task Manager details",
-"Remove Microsoft Edge shortcut from the Desktop",
-"Import Start menu layout from pre-saved reg file",
-"Show more details in file transfer dialog",
-"Turn off recently added apps on Start Menu",
-"Remove the &quot;Previous Versions&quot; tab from properties context menu",
-"Show more Windows Update restart notifications about restarting",
-"Turn off check boxes to select items",
-"Turn on acrylic taskbar transparency",
-"Always show all icons in the notification area",
-"Hide &quot;Windows Ink Workspace&quot; button in taskbar",
-"Hide Task View button on taskbar",
-"Turn off thumbnail cache removal",
-"Show accent color on the title bars and window borders",
-"Save screenshots by pressing Win+PrtScr to the Desktop",
-"Turn on ribbon in File Explorer",
-"Turn on recycle bin files delete confirmation",
-"Choose theme color for default Windows mode",
-"Turn off user first sign-in animation",
-"Let Windows try to fix apps so they're not blurry",
-"Turn off the &quot;- Shortcut&quot; name extension for new shortcuts",
-"Turn off JPEG desktop wallpaper import quality reduction",
-"Unpin Microsoft Edge and Microsoft Store from taskbar",
-"Show seconds on taskbar clock",
-"Hide People button on the taskbar",
-"Turn off Snap Assist",
-"Show Hidden Files, Folders, and Drives",
-"Show folder merge conflicts",
-"Hide &quot;Recent files&quot; in Quick access",
-"Turn off creation of an Edge shortcut on the desktop for each user profile",
-"Remove 3D Objects folder in &quot;This PC&quot; and in the navigation pane",
-"Turn off app launch tracking to improve Start menu and search results",
-"Set File Explorer to open to This PC by default"
-#endregion UI & Personalization Normalized Text
-
-#region System Normalized Text
-$textSystem = "Group svchost.exe processes",
-"Remove Windows capabilities",
-"Turn on Num Lock at startup",
-"Turn on the display of stop error information on the BSoD",
-"Always wait for the network at computer startup and logon",
-"Turn on Storage Sense to automatically free up space",
-"Set the default input method to the English language",
-"Do not allow the computer to turn off the device to save power for desktop",
-"Turn off &quot;The Windows Filtering Platform has blocked a connection&quot; message",
-"Turn off default background apps except",
-"Turn off SmartScreen for apps and files",
-"Turn on .NET 4 runtime for all apps",
-"Launch folder in a separate process",
-"Turn off hibernate",
-"Uninstall OneDrive",
-"Delete temporary files that apps aren't using",
-"Turn on automatic recommended troubleshooting",
-"Delete files in recycle bin if they have been there for over 30 days",
-"Open shortcut to the Command Prompt from Start menu as Administrator",
-"Turn off app suggestions on Start menu",
-"Turn on firewall &amp; network protection",
-"Remove printers",
-"Turn on Windows Sandbox",
-"Turn off sticky Shift key after pressing 5 times",
-"Set power management scheme for desktop and laptop",
-"Turn off Windows Script Host",
-"Set &quot;High performance&quot; in graphics performance preference for apps",
-"Automatically adjust active hours for me based on daily usage",
-"Turn on automatic backup the system registry to the &quot;C:\Windows\System32\config\RegBack&quot; folder",
-"Set location of the &quot;Desktop&quot;, &quot;Documents&quot; &quot;Downloads&quot; &quot;Music&quot;, &quot;Pictures&quot; and &quot;Videos&quot;",
-"Use the PrtScn button to open screen snipping",
-"Create old style shortcut for &quot;Devices and Printers&quot; in &quot;%AppData%\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools&quot;",
-"Turn off F1 Help key",
-"Turn on Win32 long paths",
-"Turn on Retpoline patch against Spectre v2",
-"Do not preserve zone information",
-"Change environment variable for &quot;%Temp%&quot; to &quot;%SystemDrive%\Temp&quot;",
-"Run Storage Sense every month",
-"Never delete files in &quot;Downloads&quot; folder",
-"Turn off location for this device",
-"Turn off Admin Approval Mode for administrators",
-"Turn off Windows features",
-"Turn on updates for other Microsoft products",
-"Enable System Restore",
-"Do not allow Windows 10 to manage default printer",
-"Turn on access to mapped drives from app running with elevated permissions with Admin Approval Mode enabled",
-"Set download mode for delivery optization on &quot;HTTP only&quot;",
-"Turn off Cortana"
-#endregion System Normalized Text
-
-#region Edge Normalized Text
-$textEdge = "Do not allow Microsoft Edge to pre-launch at Windows startup, when the system is idle, and each time Microsoft Edge is closed",
-"Do not allow Microsoft Edge to start and load the Start and New Tab page at Windows startup and each time Microsoft Edge is closed",
-"Turn off Windows Defender SmartScreen for Microsoft Edge"
-#endregion Edge Normalized Text
-
-#region UWP Normalized Text
-$textUwp = "Uninstall all UWP apps from all accounts"
-#endregion UWP Normalized Text
-
-#region Windows Game Recording Normalized Text
-$textGame = "Turn off Game Bar", "Turn off Game Mode", "Turn off Game Bar tips", "Turn off Windows Game Recording and Broadcasting"
-#endregion Windows Game Recording Normalized Text
-
-#region Scheduled Tasks Normalized Text
-$textTask = "Create scheduled task with the &quot;%TEMP%&quot; folder cleanup in Task Scheduler. The task runs every 62 days",
-"Create task to clean out the &quot;%SystemRoot%\SoftwareDistribution\Download&quot; folder in Task Scheduler. The task runs on Thursdays every 4 weeks",
-"Create scheduled task with the disk cleanup tool in Task Scheduler. The task runs every 90 days"
-#endregion Scheduled Tasks Normalized Text
-
-#region Microsoft Defender Normalized Text
-$textDefender = "Add folder to exclude from Windows Defender Antivirus scan",
-"Turn on Controlled folder access and add protected folders",
-"Hide notification about disabled Smartscreen for Microsoft Edge",
-"Turn on Windows Defender Sandbox",
-"Hide notification about sign in with Microsoft in the Windows Security",
-"Turn on Windows Defender Exploit Guard Network Protection",
-"Turn on Windows Defender PUA Protection"
-#endregion Microsoft Defender Normalized Text
-
-#region Context Menu Normalized Text
-$textMenu = "Remove &quot;Edit with Paint 3D&quot; from context menu",
-"Remove &quot;Include in Library&quot; from context menu",
-"Remove &quot;Create a new video&quot; from Context Menu",
-"Remove &quot;Rich Text Document&quot; from context menu",
-"Add &quot;Extract&quot; to MSI file type context menu",
-"Add &quot;Install&quot; to CAB file type context menu",
-"Remove &quot;Edit with Photos&quot; from context menu",
-"Remove &quot;Cast to Device&quot; from context menu",
-"Remove &quot;Send to&quot; from folder context menu",
-"Remove &quot;Print&quot; from batch and cmd files context menu",
-"Remove &quot;Compressed (zipped) Folder&quot; from context menu",
-"Turn off &quot;Look for an app in the Microsoft Store&quot; in &quot;Open with&quot; dialog",
-"Make the &quot;Open&quot;, &quot;Print&quot;, &quot;Edit&quot; context menu items available, when more than 15 selected",
-"Remove &quot;Bitmap image&quot; from context menu",
-"Remove &quot;Share&quot; from context menu",
-"Add &quot;Run as different user&quot; from context menu for .exe file type",
-"Remove &quot;Previous Versions&quot; from file context menu",
-"Remove &quot;Edit&quot; from Context Menu",
-"Remove &quot;Turn on BitLocker&quot; from context menu"
-#endregion Context Menu Normalized Text
-
-#region Toggle Buttons Generator
-if (Test-Path -Path "C:\Tmp\toggleButtons.txt") {
-    Remove-Item -Path "C:\Tmp\toggleButtons.txt" -Force -Confirm:$false
+if (Test-Path -Path $outFile) {
+    Remove-Item -Path $outFile -Force -Confirm:$false
+    Write-Warning -Message "File ""ToggleButtonsGenerator.txt"" deleted!"
 }
 
-$toggleSwitchName = "ToggleSwitchMenu"
+"Privacy", "Ui", "System", "Edge", "Game", "Task", "Defender", "Menu" | ForEach-Object {
+    $categoryName = $_
+    $categoryFile = "{0}\Settings-{1}-Eng.txt"-f $currentDir, $categoryName
 
-#"ToggleSwitchPrivacy" # For Privacy & Telemetry Settings
-#"ToggleSwitchUi" # For UI & Personalization Settings
-#"ToggleSwitchSystem" # For System Settings
-#"ToggleSwitchEdge" # For Edge Settings
-#"ToggleSwitchUwp" # For Uwp Settings
-#"ToggleSwitchGame" # For Windows Game Settings
-#"ToggleSwitchTasks" # For Scheduled Tasks Settings
-#"ToggleSwitchDefender" # For Microsoft Defender Settings
-#"ToggleSwitchMenu" # For Context Menu Settings
+    if (Test-Path -Path $categoryFile) {
 
-$textBlockName = "TexBlockMenu"
-#"TexBlockPrivacy" # For Privacy & Telemetry Settings
-#"TexBlockUi" # For UI & Personalization Settings
-#"TexBlockSystem" # For System Settings
-#"TexBlockEdge" # For Edge Settings
-#"TexBlockUwp" # For Uwp Settings
-#"TexBlockGame" # For Windows Game Settings
-#"TexBlockTasks" # For Scheduled Tasks Settings
-#"TexBlockDefender" # For Microsoft Defender Settings
-#"TexBlockMenu" # For Context Menu Settings
-
-for ($i = 0; $i -lt $text.Length; $i++) {
-    $content = $text[$i].Replace("""", "&quot;")
+#region Write Header
 @"
-<Border BorderBrush="{Binding ElementName=BorderPrivacy, Path=BorderBrush}" BorderThickness="{Binding ElementName=BorderPrivacy, Path=BorderThickness}" Margin="{Binding ElementName=BorderPrivacy, Path=Margin}" Style="{StaticResource BorderHoverStyle}">
+######################### $categoryName #########################
+<Border Style="{StaticResource ToggleHeaderBorder}">
+<StackPanel Orientation="Horizontal">
+<TextBlock Name="Header$categoryName" Text="$categoryName" Style="{StaticResource ToggleHeaderTextBlock}"/>
+</StackPanel>
+</Border>        
+"@ | Out-File -FilePath $outFile -Append
+#endregion Write Header        
+        
+        $text = Get-Content -Path $categoryFile
+
+        for ($i = 0; $i -lt $text.Count; $i++) {
+            
+            $string = $text[$i]
+
+            if ($string.Contains("""")) {
+                $string = $text[$i].Replace("""", "&quot;")
+            }
+
+            if ($string.Contains("&")) {
+                $string = $text[$i].Replace("&", "&amp;")
+            }
+
+            $toggleName = "ToggleSwitch_{0}_{1}"-f $categoryName, $i
+            $textBlockName = "TextBlock_{0}_{1}"-f $categoryName, $i
+
+#region Write Toggle Buttons
+@"
+<Border Style="{StaticResource ToggleBorder}">
 <StackPanel Orientation="Horizontal" Margin="5">
 <Grid HorizontalAlignment="Left">
-<ToggleButton Name="$toggleSwitchName$i" Style="{DynamicResource ToggleSwitchLeftStyle}" IsChecked="False"/>
-<TextBlock Name="$textBlockName$i" Text="$content" Margin="65 2 10 0" VerticalAlignment="Center" IsHitTestVisible="False">
+<ToggleButton Name="$toggleName" Style="{DynamicResource ToggleSwitchLeftStyle}" IsChecked="False"/>
+<TextBlock Name="$textBlockName" Text="$string" Margin="65 2 10 0" VerticalAlignment="Center" IsHitTestVisible="False">
 <TextBlock.Style>
 <Style TargetType="{x:Type TextBlock}">
 <Style.Triggers>
-<DataTrigger Binding="{Binding ElementName=$toggleSwitchName$i, Path=IsChecked}" Value="True">
+<DataTrigger Binding="{Binding ElementName=$toggleName, Path=IsChecked}" Value="True">
 <Setter Property="Foreground" Value="{Binding ElementName=BorderWindow, Path=BorderBrush}"/>
 </DataTrigger>
-<DataTrigger Binding="{Binding ElementName=$toggleSwitchName$i, Path=IsEnabled}" Value="false">
+<DataTrigger Binding="{Binding ElementName=$toggleName, Path=IsEnabled}" Value="false">
 <Setter Property="Opacity" Value="0.2" />
 </DataTrigger>
 </Style.Triggers>
@@ -217,40 +63,15 @@ for ($i = 0; $i -lt $text.Length; $i++) {
 </Grid>
 </StackPanel>
 </Border>
-"@ | Out-File -FilePath "C:\Tmp\toggleButtons.txt" -Append
+"@ | Out-File -FilePath $outFile -Append
+#endregion Write Toggle Buttons
 
-#Placeholder
-if ($i -eq ($text.Length - 1)) {
-@"
-<!--Placeholder Panel-->
-<StackPanel Height="10"/>
-"@ | Out-File -FilePath "C:\Tmp\toggleButtons.txt" -Append
-}
-}
-#endregion Toggle Buttons Generator
+        } 
 
-#region GUI with Substrate
-# @"
-# <Border BorderBrush="{Binding ElementName=BorderPrivacy, Path=BorderBrush}" BorderThickness="{Binding ElementName=BorderPrivacy, Path=BorderThickness}" Margin="{Binding ElementName=BorderPrivacy, Path=Margin}" Style="{StaticResource BorderHoverStyle}">
-# <StackPanel Orientation="Horizontal" Margin="10">
-# <Grid HorizontalAlignment="Left">
-# <ToggleButton Name="$toggleSwitchName$i" Style="{DynamicResource ToggleSwitchLeftStyle}" IsChecked="False"/>
-# <TextBlock Name="$texBoxName$i" Text="$content" Margin="65 0 10 0" VerticalAlignment="Center" IsHitTestVisible="False">
-# <TextBlock.Style>
-# <Style TargetType="{x:Type TextBlock}">
-# <Style.Triggers>
-# <DataTrigger Binding="{Binding ElementName=$toggleSwitchName$i, Path=IsChecked}" Value="True">
-# <Setter Property="Foreground" Value="{Binding ElementName=BorderWindow, Path=BorderBrush}"/>
-# </DataTrigger>
-# <DataTrigger Binding="{Binding ElementName=$toggleSwitchName$i, Path=IsEnabled}" Value="false">
-# <Setter Property="Opacity" Value="0.2" />
-# </DataTrigger>
-# </Style.Triggers>
-# </Style>
-# </TextBlock.Style>
-# </TextBlock>
-# </Grid>
-# </StackPanel>
-# </Border>
-# "@
-#endregion GUI with Substrate
+        Write-Warning -Message "File ""ToggleButtonsGenerator.txt"" created!"
+    }
+
+    else {
+        Write-Warning -Message "File ""$categoryFile"" not found!"
+    }
+}
