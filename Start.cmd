4f2b4dc3e7811807010a3364ad6dc6e63353123e
farag2 <farag2@users.noreply.github.com>
2020-05-23 17:27:46 +0300

    Add files via upload
    Fixed

--- Start.cmd
+++ Start.cmd
@@ -1,4 +1,4 @@
-﻿@echo off
+@echo off
 chcp 65001 >nul
 
-powershell.exe -ExecutionPolicy ByPass -Scope Process -NoExit -NoProfile -NoLogo -File "%~dp0Win 10.ps1"
+powershell.exe -ExecutionPolicy ByPass -NoExit -NoProfile -NoLogo -File "%~dp0Win 10.ps1"
