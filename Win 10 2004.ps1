9867cd20ad69bbe1ff8e77645f3f9f8fd04eadf9
skycommand <jadangerbrooks@hotmail.com>
2020-08-31 14:27:29 +0430

    Update Win 10 2004.ps1

--- Win 10 2004.ps1
+++ Win 10 2004.ps1
@@ -1298,7 +1298,7 @@ else
 	}
 }
 
-# Opt-in to Microsoft Update service, so that updates are offered Microsoft products other than Windows
+# Opt-in to Microsoft Update service, so that updates are offered for Microsoft products other than Windows
 # Включить автоматическое обновление для других продуктов Microsoft
 (New-Object -ComObject Microsoft.Update.ServiceManager).AddService2("7971f918-a847-4430-9279-4a52d1efe18d", 7, "")
 
