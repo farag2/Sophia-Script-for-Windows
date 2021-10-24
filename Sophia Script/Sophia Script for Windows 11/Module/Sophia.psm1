7de71e369387de8e6fb0f00f296191017df2c51b
Dmitry Nefedov <nefedovds@ya.ru>
2021-10-24 22:39:55 +0300

    Hot fix for ShareContext

--- Sophia Script/Sophia Script for Windows 11/Module/Sophia.psm1
+++ Sophia Script/Sophia Script for Windows 11/Module/Sophia.psm1
@@ -10967,15 +10967,15 @@ function ShareContext
 	{
 		"Hide"
 		{
+			Remove-Item -Path "Registry::HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\ModernSharing" -Recurse -Force -ErrorAction Ignore
+		}
+		"Show"
+		{
 			if (-not (Test-Path -Path "Registry::HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\ModernSharing"))
 			{
 				New-Item -Path "Registry::HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\ModernSharing" -Force
 			}
 			New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\ModernSharing" -Name "(default)" -PropertyType String -Value "{e2bf9676-5f8f-435c-97eb-11607a5bedf7}" -Force
-		}
-		"Show"
-		{
-			Remove-Item -Path "Registry::HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\ModernSharing" -Recurse -Force -ErrorAction Ignore
 		}
 	}
 }
