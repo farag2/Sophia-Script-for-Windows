fadf577707ccb4414bd9bb01407f03b844a1afa0
Dmitry Nefedov <nefedovds@ya.ru>
2021-10-24 19:07:38 +0300

    Update Functions.ps1

--- Sophia Script/Sophia Script for Windows 11/Functions.ps1
+++ Sophia Script/Sophia Script for Windows 11/Functions.ps1
@@ -2,8 +2,8 @@
 	.SYNOPSIS
 	The TAB completion for functions and their arguments
 
-	Version: v6.0.5
-	Date: 05.10.2021
+	Version: v6.0.6
+	Date: 24.10.2021
 
 	Copyright (c) 2014—2021 farag
 	Copyright (c) 2019—2021 farag & Inestic
@@ -54,7 +54,7 @@ function Sophia
 
 Clear-Host
 
-$Host.UI.RawUI.WindowTitle = "Sophia Script for Windows 11 v6.0.5 | Made with $([char]::ConvertFromUtf32(0x1F497)) of Windows | $([char]0x00A9) farag & Inestic, 2014$([char]0x2013)2021"
+$Host.UI.RawUI.WindowTitle = "Sophia Script for Windows 11 v6.0.6 | Made with $([char]::ConvertFromUtf32(0x1F497)) of Windows | $([char]0x00A9) farag & Inestic, 2014$([char]0x2013)2021"
 
 Remove-Module -Name Sophia -Force -ErrorAction Ignore
 Import-Module -Name $PSScriptRoot\Manifest\Sophia.psd1 -PassThru -Force
