be08ec983dbb3f55f6bf50ad88ad785265a46cca
Dmitry Nefedov <nefedovds@ya.ru>
2022-05-03 18:09:01 +0300

    Update Download_Sophia.ps1

--- Download_Sophia.ps1
+++ Download_Sophia.ps1
@@ -3,18 +3,13 @@
 	Download the latest Sophia Script version, depending on what Windows or PowerShell versions are used to
 
 	.SYNOPSIS
-
 	Download the latest Sophia Script version, depending on what Windows or PowerShell versions are used to
-
 	E.g., if you start script on Windows 11 via PowerShell 5.1 you will start downloading Sophia Script for Windows 11 PowerShell 5.1
 
-
-	E.g., if you start script on Windows 11 via PowerShell 5.1 you will start downloading Sophia Script for Windows 11 PowerShell 5.1
-
-	.EXAMPLE Download and the Sophia Script archive
+	.EXAMPLE Download and expand Sophia Script archive
 	irm script.sophi.app | iex
 
-	.EXAMPLE Download and expand the Wrapper archive
+	.EXAMPLE Download and expand Wrapper archive
 	iex "& {$(irm script.sophi.app)} -Wrapper"
 
 	.NOTES
