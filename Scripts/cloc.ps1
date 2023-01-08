a1a71a4313e4779798f64e8afecbc24d2d6b67ff
Dmitry Nefedov <nefedovds@ya.ru>
2023-01-08 18:06:14 +0300

    Update cloc.ps1

--- Scripts/cloc.ps1
+++ Scripts/cloc.ps1
@@ -1,12 +1,16 @@
 # Download cloc
 # https://github.com/AlDanial/cloc
 
+$Token = ConvertTo-SecureString '${{ secrets.GITHUB_TOKEN }}' -AsPlainText
 $Parameters = @{
-	Uri   = "https://api.github.com/repos/AlDanial/cloc/releases/latest"
+	Uri             = "https://api.github.com/repos/AlDanial/cloc/releases/latest"
+	Token           = $Token
+	Authentication  = "Bearer"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
 $Tag = (Invoke-RestMethod @Parameters).tag_name.replace("v", "")
+
 $Parameters = @{
 	Uri             = "https://github.com/AlDanial/cloc/releases/download/v$Tag/cloc-$Tag.exe"
 	OutFile         = "$PSScriptRoot\cloc.exe"
