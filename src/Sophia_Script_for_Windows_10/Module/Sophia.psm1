146b3c64a5350fc31c4d4286c296692f229755c7
Dmitry Nefedov <nefedovds@ya.ru>
2023-03-14 23:30:04 +0300

    bumped versions

--- src/Sophia_Script_for_Windows_10/Module/Sophia.psm1
+++ src/Sophia_Script_for_Windows_10/Module/Sophia.psm1
@@ -3,7 +3,7 @@
 	Sophia Script is a PowerShell module for Windows 10 & Windows 11 fine-tuning and automating the routine tasks
 
 	Version: v5.16.1
-	Date: 13.03.2023
+	Date: 14.03.2023
 
 	Copyright (c) 2014—2023 farag
 	Copyright (c) 2019—2023 farag & Inestic
