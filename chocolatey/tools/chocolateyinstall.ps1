fb951a64cb09648265dfa720935e0963e5b20b25
Dmitry Nefedov <nefedovds@ya.ru>
2025-08-04 00:50:38 +0300

    Update chocolateyinstall.ps1

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -113,7 +113,7 @@ switch ((Get-CimInstance -ClassName Win32_OperatingSystem).BuildNumber)
 		{
 			if ($packageParameters)
 			{
-				if ($packageParameters.Contains('PS7') -and )
+				if ($packageParameters.Contains('PS7'))
 				{
 					if ((Get-CimInstance -ClassName CIM_Processor).Caption -match "ARM")
 					{
@@ -124,7 +124,7 @@ switch ((Get-CimInstance -ClassName Win32_OperatingSystem).BuildNumber)
 					else
 					{
 						$LatestRelease = $JSONVersions.Sophia_Script_Windows_11_PowerShell_7
-						$URL = "https://github.com/farag2/Sophia-Script-for-Windows/releases/download/$LatestGitHubRelease/Sophia.Script.for.Windows.11.Arm.PowerShell.7.v$LatestRelease.zip"
+						$URL = "https://github.com/farag2/Sophia-Script-for-Windows/releases/download/$LatestGitHubRelease/Sophia.Script.for.Windows.11.PowerShell.7.v$LatestRelease.zip"
 						$Hash = "Hash_Sophia_Script_Windows_11_PowerShell_7"
 					}
 				}
@@ -134,7 +134,7 @@ switch ((Get-CimInstance -ClassName Win32_OperatingSystem).BuildNumber)
 				if ((Get-CimInstance -ClassName CIM_Processor).Caption -match "ARM")
 				{
 					$LatestRelease = $JSONVersions.Sophia_Script_Windows_11_PowerShell_5_1
-					$URL = "https://github.com/farag2/Sophia-Script-for-Windows/releases/download/$LatestGitHubRelease/Sophia.Script.for.Windows.11.v$LatestRelease.zip"
+					$URL = "https://github.com/farag2/Sophia-Script-for-Windows/releases/download/$LatestGitHubRelease/Sophia.Script.for.Windows.11.Arm.v$LatestRelease.zip"
 					$Hash = "Hash_Sophia_Script_Windows_11_Arm_PowerShell_5_1"
 				}
 				else
