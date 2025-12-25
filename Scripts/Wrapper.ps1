13fa407646522a5a9a9958698103999a08c6e493
Dmitry Nefedov <nefedovds@ya.ru>
2025-12-25 17:59:16 +0300

    Update Wrapper.ps1

--- Scripts/Wrapper.ps1
+++ Scripts/Wrapper.ps1
@@ -8,7 +8,7 @@ $Latest_Release_Sophia_Script_Wrapper = (Invoke-RestMethod @Parameters).Sophia_S
 
 Write-Verbose -Message "Sophia.Script.Wrapper.v$Latest_Release_Sophia_Script_Wrapper.zip" -Verbose
 
-New-Item -Path "Sophia_Script_Wrapper_v$Latest_Release_Sophia_Script_Wrapper" -ItemType Directory -Force
+New-Item -Path "Sophia_Script\Sophia_Script_Wrapper_v$Latest_Release_Sophia_Script_Wrapper" -ItemType Directory -Force
 
 # Copy Wrapper to new folder
 Get-ChildItem -Path Wrapper -Exclude README.md -Force | Copy-Item -Destination "Sophia_Script\Sophia_Script_Wrapper_v$Latest_Release_Sophia_Script_Wrapper" -Recurse -Force
