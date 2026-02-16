49ebc8445dfc63acbbcc7b718006a00434b17b3e
Dmitry Nefedov <nefedovds@ya.ru>
2026-02-16 15:16:21 +0300

    Fixed typos

--- src/Sophia_Script_for_Windows_11_ARM/Module/Sophia.psm1
+++ src/Sophia_Script_for_Windows_11_ARM/Module/Sophia.psm1
@@ -7932,7 +7932,7 @@ function RegistryBackup
 		$Disable
 	)
 
-	Remove-ItemProperty -Path "HKLM:\SOFTWARE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance" -Name MaintenanceDisabled -Force -ErrorAction Ignore
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance" -Name MaintenanceDisabled -Force -ErrorAction Ignore
 	Get-ScheduledTask -TaskName RegIdleBackup -ErrorAction Ignore | Enable-ScheduledTask -ErrorAction Ignore
 
 	switch ($PSCmdlet.ParameterSetName)
